c02aae06517dbd5ee870408b2b7adc2df93ff50a
user <user@192.168.56.1>
2022-04-15 14:49:16 +0000

    constructeur

--- gestion-nom/src/gestion_nom/Etudiant.java
+++ gestion-nom/src/gestion_nom/Etudiant.java
@@ -4,4 +4,12 @@ public class Etudiant {
      private int id;
      private String nom;
      private double note;
+     public Etudiant() {
+		this.id=id;this.nom=nom;this.note=10:
+	}
+     public String toString()
+     {
+    	 return "("+this.nom +":" +this.note+")";
+     }
+     
 }
