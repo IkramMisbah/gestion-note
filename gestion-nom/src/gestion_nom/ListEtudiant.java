bf3a1bbd9bc2333948f62dbbab0b5ce51b3a014a
user <user@192.168.56.1>
2022-04-15 15:51:39 +0000

    add

--- gestion-nom/src/gestion_nom/ListEtudiant.java
+++ gestion-nom/src/gestion_nom/ListEtudiant.java
@@ -1,7 +1,11 @@
 package gestion_nom;
-import java.util.ArrayList; 
+import java.util.ArrayList;
+
+ 
 public class ListEtudiant extends ArrayList<Etudiant>
     {
-	
-
+	  public boolean add(Etudiant e) {
+		  if(this.contains(e)) return false;
+		  return super.add(e);
+	  }
     }
