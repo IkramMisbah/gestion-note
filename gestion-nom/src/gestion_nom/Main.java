e79c1075d41ab9c2647daf4ef9c6067589cd3ee6
user <user@192.168.56.1>
2022-04-15 16:10:38 +0000

    creer etudiant

--- gestion-nom/src/gestion_nom/Main.java
+++ gestion-nom/src/gestion_nom/Main.java
@@ -3,7 +3,9 @@ package gestion_nom;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Etudiant e1 =new Etudiant(1, "hamid" );
+		Etudiant e2 =new Etudiant(2, "driss" );
+		Etudiant e2 =new Etudiant(1, "youssef" );
 
 	}
 
